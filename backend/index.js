a959994225c1441d039cf68d41cbe6fda4169835
yash chokhani <yashchokhani95@gmail.com>
2023-08-12 15:05:33 +0530

    added the basic backend file structure

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("*", (req, res) => {
+  res.status(400).send("Invalid request");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
