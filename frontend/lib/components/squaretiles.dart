a7d6ecd03049bd24d6eb65672f382e462c6c450b
ayush pandit <ayushpanditmoto@gmail.com>
2023-08-12 12:10:14 +0530

    feat: components button added :sparkles:

---
+++ frontend/lib/components/squaretiles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  const SquareTile({super.key, required this.imagePath, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[200],
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            imagePath,
+            height: 40,
+          ),
+          const SizedBox(width: 20),
+          Text(title,
+              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+}
