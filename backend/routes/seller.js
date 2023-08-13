8a00e8787d7f9c966969ecb3efd7206c25d0c863
yash chokhani <yashchokhani95@gmail.com>
2023-08-13 09:39:22 +0530

    corrected api error

--- backend/routes/seller.js
+++ backend/routes/seller.js
@@ -3,26 +3,23 @@ const Seller = require("../models/seller");
 const SolarPanel = require("../models/solarPanel");
 const { isLoggedIn } = require("../middlewares");
 const router = express.Router();
-const upload = require('../middleware/multer');
-const sharp = require('sharp');
-const AWS=require('aws-sdk');
-const {v4 : uuidv4} = require('uuid')
+const upload = require("../middleware/multer");
+const sharp = require("sharp");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
 
+BUCKET_NAME = process.env.BUCKET_NAME;
+BUCKET_REGION = process.env.BUCKET_REGION;
+ACCESS_KEY = process.env.ACCESS_KEY;
+SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
 
-BUCKET_NAME=process.env.BUCKET_NAME;
-BUCKET_REGION=process.env.BUCKET_REGION;    
-ACCESS_KEY=process.env.ACCESS_KEY;
-SECRET_ACCESS_KEY=process.env.SECRET_ACCESS_KEY;
-
-const s3=new AWS.S3({
-    credentials:{
-        accessKeyId: ACCESS_KEY,
-        secretAccessKey: SECRET_ACCESS_KEY,
-    },
-    region:BUCKET_REGION
-})
-
-
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+  region: BUCKET_REGION,
+});
 
 router.get("/", async (req, res) => {
   const sellers = await Seller.find({});
@@ -100,8 +97,12 @@ router.post("/:id/solarPanel", isLoggedIn, async (req, res) => {
   } = req.body;
   const userId = req.params.id;
   const seller = await Seller.findOne({ userId });
-  const solarPanel = await SolarPanel.findOne({ seller: seller._id });
-  if (solarPanel && solarPanel.brand === brand && solarPanel.model === model) {
+  const solarPanel = await SolarPanel.findOne({
+    seller: seller._id,
+    brand,
+    model,
+  });
+  if (solarPanel) {
     return res.status(400).send("Solar Panel already exists");
   }
   const newSolarPanel = new SolarPanel({
@@ -123,47 +124,38 @@ router.post("/:id/solarPanel", isLoggedIn, async (req, res) => {
   });
 });
 
+router.get("/:id/image", isLoggedIn, async (req, res) => {
+  const userId = req.params.userId;
+  const user = Seller.find({ userId });
+  user.image = req.body.image;
+  await user.save();
+  res.send("Image added successfully");
+});
 
-
-router.post("/uploadImage",upload.single("image"),async (req,res)=>{
-
+router.post("/uploadImage", upload.single("image"), async (req, res) => {
   try {
-    
     const newId = uuidv4();
-    console.log(req.file.buffer)
-    const buffer= await sharp(req.file.buffer).resize({height:1920,width:1080,fit:"contain"}).toBuffer();
-    const params={
-        Bucket:BUCKET_NAME+`/main/user/solar`,
-        Key: newId,
-        Body: buffer,
-        ContentType: req.file.mimetype,
-        ACL:'public-read'
-    }
+    console.log(req.file.buffer);
+    const buffer = await sharp(req.file.buffer)
+      .resize({ height: 1920, width: 1080, fit: "contain" })
+      .toBuffer();
+    const params = {
+      Bucket: BUCKET_NAME + `/main/user/solar`,
+      Key: newId,
+      Body: buffer,
+      ContentType: req.file.mimetype,
+      ACL: "public-read",
+    };
     await s3.putObject(params).promise();
-    imageUrl=`https://${BUCKET_NAME}.s3.amazonaws.com/main/user/solar/${newId}`;
-    
+    imageUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/main/user/solar/${newId}`;
+
     res.send(imageUrl);
-} catch (err) {
+  } catch (err) {
     res.send({
-        success: false,
-        msg: err.message || "Error Occurred while uploading image",
+      success: false,
+      msg: err.message || "Error Occurred while uploading image",
     });
-}
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+});
 
 module.exports = router;
